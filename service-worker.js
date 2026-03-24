2ccb9a469565fd28a6797b10568b043606332787
buffetsense22 <buffetsense@gmail.com>
2026-03-24 10:33:34 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', e => {
+  console.log("Service Worker Installed");
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => {
+    return new Response("Offline");
+  }));
+});
