c54eff6457dfd5fadfd7495469dece5a7abee5ac
buffetsense22 <buffetsense@gmail.com>
2026-03-24 10:35:01 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,20 @@
-self.addEventListener('install', e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "mlsn-cache-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => {
-    return new Response("Offline");
-  }));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
